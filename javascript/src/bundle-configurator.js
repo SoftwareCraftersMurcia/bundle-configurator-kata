cbcb77f5912c52721251b1f8912b9e9bcc705c5d
Carlos Castillo <castillo.st@gmail.com>
2022-04-29 19:13:46 +0200

    Add logic to find bundle p1,p2

--- javascript/src/bundle-configurator.js
+++ javascript/src/bundle-configurator.js
@@ -1,9 +1,14 @@
+const bundles = {
+  B1: ["P1", "P2"]
+};
+
 const selectProducts = (productNames) => {
   return "P1";
 };
 
 const bundleChecker = (productList) => {
-  return productList;
+  const bundle = Object.keys(bundles).find((key) => bundles[key].every(p => productList.includes(p)))
+  return bundle?[bundle]:productList;
 };
 
 module.exports = { selectProducts, bundleChecker };
--- javascript/test/bundle-configurator.test.js
+++ javascript/test/bundle-configurator.test.js
@@ -11,5 +11,11 @@ describe("Bundle Checker", () => {
   it("There is no bundle for [P1]", () => {
     const actual = bundleChecker(["P1"]);
     const expected = ["P1"];
+    expect(actual).toEqual(expected);
+  });
+  it("should be bundle [B1] bundle for [P1,P2]", () => {
+    const actual = bundleChecker(["P1","P2"]);
+    const expected = ["B1"];
+    expect(actual).toEqual(expected);
   });
 });
